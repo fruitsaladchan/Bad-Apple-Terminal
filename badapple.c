efd62e65bdaa3d61ea57dbc4eec7b803fe645eb3
fruit salad <124645742+fruitsaladchan@users.noreply.github.com>
2024-12-30 14:11:32 -0500

    remove pause key

--- badapple.c
+++ badapple.c
@@ -5,8 +5,6 @@
 #include <locale.h>
 #include <signal.h>
 #include <time.h>
-#include <termios.h>
-#include <fcntl.h>
 #include <SDL2/SDL.h>
 
 #define WIDTH 180
@@ -14,48 +12,22 @@
 #define FRAME_SIZE (WIDTH * HEIGHT)
 #define AUDIO_BUFFER_SIZE 4096
 #define FPS 30
-#define FRAME_TIME (1000000 / FPS)
+#define FRAME_TIME (1000000 / FPS)  // Time per frame in microseconds
 
 SDL_AudioDeviceID audio_device;
 Uint8* audio_buffer;
 Uint32 audio_length;
 Uint32 audio_pos = 0;
 volatile int running = 1;
-volatile int paused = 0;
-struct timespec start_time, pause_time;
-unsigned long total_pause_time = 0;
-unsigned long frame_count = 0;
 
-// Terminal settings
-struct termios orig_termios;
+struct timespec start_time;
+unsigned long frame_count = 0;
 
 void signal_handler(int signum) {
     running = 0;
 }
 
-// Function to make terminal read non-blocking
-void set_nonblocking_input() {
-    struct termios new_termios;
-    tcgetattr(STDIN_FILENO, &orig_termios);
-    new_termios = orig_termios;
-    new_termios.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);
-    
-    // Set non-blocking
-    int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
-    fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
-}
-
-void restore_terminal() {
-    tcsetattr(STDIN_FILENO, TCSANOW, &orig_termios);
-}
-
 void audio_callback(void* userdata, Uint8* stream, int len) {
-    if (paused) {
-        memset(stream, 0, len);
-        return;
-    }
-    
     if (audio_pos + len > audio_length) {
         len = audio_length - audio_pos;
     }
@@ -83,7 +55,7 @@ void move_cursor(int rows) {
     printf("\033[%dB", rows);
 }
 
-void reset_terminal_display() {
+void reset_terminal() {
     printf("\033[2J");
     printf("\033[H");
     printf("\033[?25h");
@@ -102,13 +74,6 @@ void render_frame(const unsigned char* frame_data) {
     clear_screen();
     move_cursor(2);
     
-    // Show pause status
-    if (paused) {
-        printf("  [PAUSED] Press SPACE to resume\n");
-    } else {
-        printf("  Press SPACE to pause\n");
-    }
-    
     for (int y = 0; y < HEIGHT; y++) {
         printf("  ");
         for (int x = 0; x < WIDTH; x++) {
@@ -126,30 +91,10 @@ unsigned long get_microseconds() {
            (current_time.tv_nsec - start_time.tv_nsec) / 1000;
 }
 
-void check_keyboard_input() {
-    char c;
-    if (read(STDIN_FILENO, &c, 1) > 0) {
-        if (c == ' ') {  // Spacebar
-            paused = !paused;
-            if (paused) {
-                clock_gettime(CLOCK_MONOTONIC, &pause_time);
-            } else {
-                struct timespec resume_time;
-                clock_gettime(CLOCK_MONOTONIC, &resume_time);
-                total_pause_time += (resume_time.tv_sec - pause_time.tv_sec) * 1000000 +
-                                  (resume_time.tv_nsec - pause_time.tv_nsec) / 1000;
-            }
-        }
-    }
-}
-
 int main() {
     signal(SIGINT, signal_handler);
     setlocale(LC_ALL, "en_US.UTF-8");
     printf("\033[?25l");
-    
-    // Set up non-blocking input
-    set_nonblocking_input();
 
     if (SDL_Init(SDL_INIT_AUDIO) < 0) {
         printf("SDL initialization failed: %s\n", SDL_GetError());
@@ -185,40 +130,34 @@ int main() {
         return 1;
     }
 
+    // Wait for 150ms before starting
     usleep(150000);
+
     clock_gettime(CLOCK_MONOTONIC, &start_time);
+    
     SDL_PauseAudioDevice(audio_device, 0);
 
     while (running) {
         audio_pos = 0;
         frame_count = 0;
-        total_pause_time = 0;
         clock_gettime(CLOCK_MONOTONIC, &start_time);
 
         for (int frame = 0; frame < total_frames && running; frame++) {
-            check_keyboard_input();
+            unsigned long target_time = frame_count * FRAME_TIME;
+            unsigned long current_time = get_microseconds();
             
-            if (!paused) {
-                unsigned long target_time = frame_count * FRAME_TIME;
-                unsigned long current_time = get_microseconds() - total_pause_time;
-                
-                if (target_time > current_time) {
-                    usleep(target_time - current_time);
-                }
-
-                fseek(fp, frame * FRAME_SIZE, SEEK_SET);
-                if (fread(frame_data, 1, FRAME_SIZE, fp) != FRAME_SIZE) {
-                    printf("Failed to read frame %d\n", frame);
-                    break;
-                }
-                
-                render_frame(frame_data);
-                frame_count++;
-            } else {
-                render_frame(frame_data);
-                usleep(33333);  // Reduced CPU usage while paused
-                frame--;  // Stay on current frame while paused
+            if (target_time > current_time) {
+                usleep(target_time - current_time);
             }
+
+            fseek(fp, frame * FRAME_SIZE, SEEK_SET);
+            if (fread(frame_data, 1, FRAME_SIZE, fp) != FRAME_SIZE) {
+                printf("Failed to read frame %d\n", frame);
+                break;
+            }
+            
+            render_frame(frame_data);
+            frame_count++;
         }
         
         if (running) {
@@ -226,8 +165,7 @@ int main() {
         }
     }
 
-    /* reset_terminal_display(); */
-    restore_terminal();
+ /*   reset_terminal(); */
     free(frame_data);
     fclose(fp);
     SDL_CloseAudioDevice(audio_device);
